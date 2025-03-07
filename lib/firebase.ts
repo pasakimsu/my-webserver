6176301ef2f7022aa4d904ae873d70babf6f80f0
pasakimsu <nalb@naver.com>
2025-03-08 02:44:19 +0900

    update4

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, query, where, doc, setDoc, addDoc, deleteDoc, 
-    orderBy, startAt, endAt
- } from "firebase/firestore";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    query,
+    where,
+    doc,
+    setDoc,
+    addDoc,
+    deleteDoc,
+    orderBy,
+    startAt,
+    endAt,
+    getDoc // ✅ getDoc 추가
+} from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAndxmjKRA3ZDrOZ0aALjnTPFHC7Q-jpXw",
@@ -12,8 +24,23 @@ const firebaseConfig = {
     appId: "1:545475332778:web:c879423ce3010694805ed3"
 };
 
-// Firebase 초기화
+// ✅ Firebase 초기화
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, getDocs, query, where, doc, setDoc, addDoc, deleteDoc, orderBy, startAt, endAt };
+// ✅ Firebase 관련 함수 및 객체 export
+export {
+    db,
+    collection,
+    getDocs,
+    query,
+    where,
+    doc,
+    setDoc,
+    addDoc,
+    deleteDoc,
+    orderBy,
+    startAt,
+    endAt,
+    getDoc // ✅ getDoc 추가
+};
